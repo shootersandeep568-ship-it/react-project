c0ec061655bb15846225ac13e70e0dd4d3093fe8
sandeep <shootersandeep568@gmail.com>
2026-03-20 12:43:33 +0530

    log

--- src/componets/Homepage/SliderData.js
+++ src/componets/Homepage/SliderData.js
@@ -18,7 +18,7 @@ const categorydata = [
         category: "fashion",
         image: "https://plus.unsplash.com/premium_photo-1664202526559-e21e9c0fb46a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     },
-    ,
+    
     {
         id: 3,
         title: "fashion",
