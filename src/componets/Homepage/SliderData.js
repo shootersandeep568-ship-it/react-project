a9c8dcae3848f51dfcf3dd2a8116c96377acaf5a
sandeep <shootersandeep568@gmail.com>
2026-04-03 08:54:17 +0530

    new

--- src/componets/Homepage/SliderData.js
+++ src/componets/Homepage/SliderData.js
@@ -628,114 +628,6 @@ const categorydata = [
         image: "https://plus.unsplash.com/premium_photo-1681816189679-fa02d1acd1de?q=80&w=1177&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     }, 
     {
-        id: 70,
-        title: "Electronics",
-        description: "createdescription",
-        price: 500,
-        selectsize: "xl",
-        category: "Electronics",
-        image: "https://images.unsplash.com/photo-1550009158-9ebf69173e03?q=80&w=1201&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }, 
-    {
-        id: 71,
-        title: "Electronics",
-        description: "createdescription",
-        price: 500,
-        selectsize: "xl",
-        category: "Electronics",
-        image: "https://plus.unsplash.com/premium_photo-1681816189679-fa02d1acd1de?q=80&w=1177&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    },
-    {
-        id: 72,
-        title: "Electronics",
-        description: "createdescription",
-        price: 500,
-        selectsize: "xl",
-        category: "Electronics",
-        image: "https://images.unsplash.com/photo-1550009158-9ebf69173e03?q=80&w=1201&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }, 
-    {
-        id: 73,
-        title: "Electronics",
-        description: "createdescription",
-        price: 500,
-        selectsize: "xl",
-        category: "Electronics",
-        image: "https://plus.unsplash.com/premium_photo-1681816189679-fa02d1acd1de?q=80&w=1177&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }, 
-    {
-        id: 74,
-        title: "Electronics",
-        description: "createdescription",
-        price: 500,
-        selectsize: "xl",
-        category: "Electronics",
-        image: "https://images.unsplash.com/photo-1550009158-9ebf69173e03?q=80&w=1201&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }, 
-    {
-        id: 75,
-        title: "Electronics",
-        description: "createdescription",
-        price: 500,
-        selectsize: "xl",
-        category: "Electronics",
-        image: "https://plus.unsplash.com/premium_photo-1681816189679-fa02d1acd1de?q=80&w=1177&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }, 
-    {
-        id: 76,
-        title: "Electronics",
-        description: "createdescription",
-        price: 500,
-        selectsize: "xl",
-        category: "Electronics",
-        image: "https://images.unsplash.com/photo-1550009158-9ebf69173e03?q=80&w=1201&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }, 
-    {
-        id: 77,
-        title: "Electronics",
-        description: "createdescription",
-        price: 500,
-        selectsize: "xl",
-        category: "Electronics",
-        image: "https://plus.unsplash.com/premium_photo-1681816189679-fa02d1acd1de?q=80&w=1177&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }, 
-    {
-        id: 78,
-        title: "Electronics",
-        description: "createdescription",
-        price: 500,
-        selectsize: "xl",
-        category: "Electronics",
-        image: "https://images.unsplash.com/photo-1550009158-9ebf69173e03?q=80&w=1201&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }, 
-    {
-        id: 79,
-        title: "Electronics",
-        description: "createdescription",
-        price: 500,
-        selectsize: "xl",
-        category: "Electronics",
-        image: "https://plus.unsplash.com/premium_photo-1681816189679-fa02d1acd1de?q=80&w=1177&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }, 
-    {
-        id: 80,
-        title: "Electronics",
-        description: "createdescription",
-        price: 500,
-        selectsize: "xl",
-        category: "Electronics",
-        image: "https://images.unsplash.com/photo-1550009158-9ebf69173e03?q=80&w=1201&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }, 
-    {
-        id: 81,
-        title: "Electronics",
-        description: "createdescription",
-        price: 500,
-        selectsize: "xl",
-        category: "Electronics",
-        image: "https://plus.unsplash.com/premium_photo-1681816189679-fa02d1acd1de?q=80&w=1177&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    },
-    {
         id: 82,
         title: "TVs & Appliances",
         description: "createdescription",
@@ -798,24 +690,7 @@ const categorydata = [
         category: "TVs & Appliances",
         image: "https://plus.unsplash.com/premium_photo-1664302386657-708e15ab9691?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     }, 
-    {
-        id: 89,
-        title: "TVs & Appliances",
-        description: "createdescription",
-        price: 500,
-        selectsize: "xl",
-        category: "TVs & Appliances",
-        image: "https://plus.unsplash.com/premium_photo-1661476586623-1db68d015e45?q=80&w=1331&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }, 
-    {
-        id: 90,
-        title: "TVs & Appliances",
-        description: "createdescription",
-        price: 500,
-        selectsize: "xl",
-        category: "TVs & Appliances",
-        image: "https://plus.unsplash.com/premium_photo-1664302386657-708e15ab9691?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }, 
+   
     {
         id: 91,
         title: "TVs & Appliances",
@@ -888,24 +763,7 @@ const categorydata = [
         category: "Home & Furniture",
         image: "https://media.istockphoto.com/id/1182454657/photo/bohemian-living-room-interior-3d-render.jpg?s=1024x1024&w=is&k=20&c=b7s1rUyB39kGKJWJPqLJTQqYGbGCXtjOJA13zJR58X8="
     }, 
-    {
-        id: 99,
-        title: "Home & Furniture",
-        description: "createdescription",
-        price: 500,
-        selectsize: "xl",
-        category: "Home & Furniture",
-        image: "https://media.istockphoto.com/id/1344082102/photo/large-glass-enclosed-balcony.jpg?s=1024x1024&w=is&k=20&c=kXFNltbMNvKktC_tgH9ypUCeOErYdpP42Pbc2EheJqw="
-    }, 
-    {
-        id: 100,
-        title: "Home & Furniture",
-        description: "createdescription",
-        price: 500,
-        selectsize: "xl",
-        category: "Home & Furniture",
-        image: "https://media.istockphoto.com/id/1182454657/photo/bohemian-living-room-interior-3d-render.jpg?s=1024x1024&w=is&k=20&c=b7s1rUyB39kGKJWJPqLJTQqYGbGCXtjOJA13zJR58X8="
-    }, 
+    
     {
         id: 101,
         title: "Home & Furniture",
@@ -962,24 +820,6 @@ const categorydata = [
     }, 
     {
         id: 107,
-        title: "Flight Bookings",
-        description: "createdescription",
-        price: 500,
-        selectsize: "xl",
-        category: "Flight Bookings",
-        image: "https://plus.unsplash.com/premium_photo-1679758630312-a3d601c411d8?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }, 
-    {
-        id: 108,
-        title: "Flight Bookings",
-        description: "createdescription",
-        price: 500,
-        selectsize: "xl",
-        category: "Flight Bookings",
-        image: 'https://plus.unsplash.com/premium_photo-1679758630196-a284f1466313?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-    }, 
-    {
-        id: 109,
         title: "Flight Bookings",
         description: "createdescription",
         price: 500,
@@ -1077,24 +917,7 @@ const categorydata = [
         category: "Beauty, Food..",
         image: "https://plus.unsplash.com/premium_photo-1663841166571-2bc77d85ef68?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     }, 
-    {
-        id: 120,
-        title: "Beauty, Food..",
-        description: "createdescription",
-        price: 500,
-        selectsize: "xl",
-        category: "Beauty, Food..",
-        image: "https://plus.unsplash.com/premium_photo-1663841165450-93c86fe4dabe?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    },
-     {
-        id: 121,
-        title: "Beauty, Food..",
-        description: "createdescription",
-        price: 500,
-        selectsize: "xl",
-        category: "Beauty, Food..",
-        image: "https://plus.unsplash.com/premium_photo-1663841166571-2bc77d85ef68?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    },
+   
 
 ]
 
